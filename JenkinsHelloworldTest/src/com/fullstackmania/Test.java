28ec239a9cf53def65efd1b3a71201cb8b29fee2
Sourav  Panigrahi <50149081+sourav123456789@users.noreply.github.com>
2021-09-30 19:51:15 +0530

    Update Test.java

--- JenkinsHelloworldTest/src/com/fullstackmania/Test.java
+++ JenkinsHelloworldTest/src/com/fullstackmania/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("Hello world");
 		System.out.println("Hello world from webhook");
 		System.out.println("Hello world from webhook1");
+		System.out.println("Hello world from webhook2");
 	}
 
 }
